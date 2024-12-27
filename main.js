7450dddf65dc23d000bab1abf43944e7429f7407
Khens Code <davidsamwelson@gmail.com>
2024-12-27 14:18:39 +0200

    main.js

---
+++ main.js
@@ -0,0 +1,15 @@
+document.addEventListener("DOMContentLoaded" , function(){ const body = document.querySelector('body');
+const btn = document.querySelector('#btn');
+const h1 = document.querySelector('h1');
+
+btn.addEventListener('click' ,() => {
+let random1 = Math.floor(Math.random()*255);
+let random2 = Math.floor(Math.random()*255);
+let random3 = Math.floor(Math.random()*255);
+let colorValue =
+'rgb(' + random1 + "," + random2 + "," + random3 +")";
+body.style.background = colorValue;
+h1.innerHTML = colorValue;
+});
+
+})
